5d9d9e196b96efc36bef05860374393e190f1f21
leonardopetreca <61334273+leonardopetreca@users.noreply.github.com>
2023-04-15 21:27:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def sendGmail(lista_dados):
     
     
     print(content)
-    yag = yagmail.SMTP(user="andre@marketinglabs.com.br", password=PASSWORD)
+    yag = yagmail.SMTP(user="andre@marketinglabs.com.br", password=PASSWORD,  host='smtp.gmail.com')
     yag.send(to=receiver, subject=Subject, contents=content)
 
 
