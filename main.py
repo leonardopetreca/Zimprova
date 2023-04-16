15667302493b8f219ba49f3b63d78bb2d6a2d887
leonardopetreca <61334273+leonardopetreca@users.noreply.github.com>
2023-04-15 22:40:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -323,5 +323,6 @@ def home_calc():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run()
+#debug=True, port=os.getenv("PORT", default=5000)
      
