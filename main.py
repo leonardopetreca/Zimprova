6d79d5c338bc3122e4b00dad16afa535d944660f
leonardopetreca <61334273+leonardopetreca@users.noreply.github.com>
2023-04-19 23:13:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def sendGmail(lista_dados):
     '''
     
     
-     with app.app_context():
+    with app.app_context():
         msg = Message( Subject,sender,recipients =['andre@marketinglabs.com.br'] )
         msg.body = content
         mail.send(msg)
